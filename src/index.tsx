39da554344bfeb5badc4050e124053243a6ee3fe
eggplants <w10776e8w@yahoo.co.jp>
2022-04-19 14:23:20 +0900

    fix: make error messages more verbose

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,17 @@
 import { useState } from "react";
-import { URI } from "./constraints/config"
-import ReactDOM from "react-dom"
+import { URI } from "./constraints/config";
+import ReactDOM from "react-dom";
 import gql from "graphql-tag";
-import MonacoEditor from "react-monaco-editor"
-import "./index.css";
+import "monaco-graphql";
+import MonacoEditor from "react-monaco-editor";
 
-
-import { getClient } from "./lib/query"
+import { getClient } from "./lib/query";
 
 const App = () => {
-  const [endpoint, setEndpoint] = useState(URI)
-  const [inputValue, setInputValue] = useState(`
+  const [endpoint, setEndpoint] = useState(URI);
+  const [inputValue, setInputValue] = useState(
+    // eslint-disable-line @typescript-eslint/no-unused-vars
+    `
 query ($id: Int) {
   Media (id: $id, type: ANIME) {
     id
@@ -21,64 +22,82 @@ query ($id: Int) {
     }
   }
 }
-  `.trim())
-  const [resultValue, setResultValue] = useState("")
-  const [buttonDisabled, setButtonDisabled] = useState(false)
-  const [status, setStatus] = useState("Ready!")
+  `.trim()
+  );
+  const [resultValue, setResultValue] = useState("");
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+  const [status, setStatus] = useState("Ready!");
 
+  const client = getClient(endpoint);
 
   const query = () => {
-    let client = getClient(endpoint);
-    setStatus("loading...")
-    setButtonDisabled(true)
+    setStatus("loading...");
+    setButtonDisabled(true);
     client
-      .query({ query: gql`${inputValue}`,  })
-      .then(r => {
-        setResultValue(JSON.stringify(r, null, 2))
-        setStatus("success!")
-        setButtonDisabled(false)
+      .query({
+        query: gql`
+          ${inputValue}
+        `,
       })
-      .catch(err => {
-        alert(err)
-        setStatus("failed...")
-        setButtonDisabled(true)
+      .then((r) => {
+        setResultValue(JSON.stringify(r, null, 2));
+        setStatus("success!");
+        setButtonDisabled(false);
       })
-  }
+      .catch((err) => {
+        alert(err);
+        setStatus("failed...");
+        setButtonDisabled(true);
+      });
+  };
   return (
     <>
       <h1>Query form</h1>
       <div className="endpoint-input">
         <label>
           Endpoint:
-          <input type="text" value={endpoint} onChange={(e) => setEndpoint(e.target.value)} />
+          <input
+            type="text"
+            value={endpoint}
+            onChange={(e) => setEndpoint(e.target.value)}
+          />
         </label>
       </div>
       <div className="query-io">
         <div className="query-input">
-          <label>Query:
+          <label>
+            Query:
             <MonacoEditor
-              value={inputValue} language="graphql"
+              value={inputValue}
+              language="graphql"
               width="800"
               height="300"
               options={{ automaticLayout: true, scrollBeyondLastLine: false }}
             />
           </label>
-          <button disabled={buttonDisabled} onClick={query}>{status}</button>
+          <button disabled={buttonDisabled} onClick={query}>
+            {status}
+          </button>
         </div>
         <div className="query-output">
-          <label>Result:
+          <label>
+            Result:
             <MonacoEditor
               value={resultValue}
               width="800"
               height="300"
               language="graphql"
-              options={{ automaticLayout: true, readOnly: true, scrollBeyondLastLine: false }}
+              options={{
+                automaticLayout: true,
+                readOnly: true,
+                scrollBeyondLastLine: false,
+              }}
             />
           </label>
         </div>
       </div>
     </>
   );
-}
+};
 
-ReactDOM.render(<App />, document.getElementById("root"))
+ReactDOM.render(<App />, document.getElementById("root"));
--- src/lib/query.ts
+++ src/lib/query.ts
@@ -1,27 +1,42 @@
-import { ApolloClient, ApolloLink, NormalizedCacheObject } from "@apollo/client";
+import {
+  ApolloClient,
+  ApolloLink,
+  NormalizedCacheObject,
+} from "@apollo/client";
 import { InMemoryCache } from "@apollo/client";
 import { onError } from "@apollo/client/link/error";
-import ApolloLinkTimeout from 'apollo-link-timeout';
+import ApolloLinkTimeout from "apollo-link-timeout";
 import { HttpLink } from "@apollo/client/link/http";
 import { URI } from "../constraints/config";
 
-export const getClient = (endpoint?: string): ApolloClient<NormalizedCacheObject> => {
-  const httpLink = new HttpLink({
-    uri: endpoint || URI,
-    fetch: fetch,
-  });
-  const cache = new InMemoryCache();
+export const getClient = (
+  endpoint?: string
+): ApolloClient<NormalizedCacheObject> => {
   const errorLink = onError(({ graphQLErrors, networkError }) => {
     if (graphQLErrors)
-      graphQLErrors.map(({ message, locations, path }) =>
+      graphQLErrors.map(({ message, locations, path, extensions, positions }) =>
         console.log(
-          `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+          [
+            "[GraphQL error]:",
+            `- Message: ${message}`,
+            `- Location: ${locations}`,
+            `- Path: ${path}`,
+            `- Error Code: ${extensions.code}`,
+            `- Position: ${positions?.toString()}`,
+          ].join("\n")
         )
       );
     if (networkError) console.log(`[Network error]: ${networkError}`);
   });
+  const httpLink = new HttpLink({
+    uri: endpoint || URI,
+    fetch: fetch,
+  });
   const timeoutLink = new ApolloLinkTimeout(10000);
+
   const link = ApolloLink.from([errorLink, httpLink, timeoutLink]);
+  const cache = new InMemoryCache();
   const client = new ApolloClient({ link, cache });
+
   return client;
 };
